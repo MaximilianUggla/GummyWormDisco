21921027e1a06abc6c2f6e5c9b8a163f409244a6
MaximilianUggla <maxi.uggla@icloud.com>
2025-11-13 21:26:43 +0100

    Added DanceSimulator, might turn this into a monitor

---
+++ DanceSimulator.java
@@ -0,0 +1,11 @@
+package GummyWormDisco;
+
+public class DanceSimulator {
+    
+    public DanceSimulator(String[][] board, Worm[] worms, String[] beats) {}
+
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
