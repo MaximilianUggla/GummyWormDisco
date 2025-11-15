aef3687e5a323689444be4d55ee3a7f68e07e085
MaximilianUggla <maxi.uggla@icloud.com>
2025-11-15 16:07:26 +0100

    made cordinates comparable

--- Coordinate.java
+++ Coordinate.java
@@ -1,6 +1,6 @@
 package GummyWormDisco;
 
-public class Coordinate {
+public class Coordinate implements Comparable<Coordinate> {
     private int x, y;
 
     public Coordinate(int x, int y) {
@@ -22,8 +22,28 @@ public class Coordinate {
     public int y() {return y;}
 
     @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Coordinate other) {
+            return x() == other.x() && y() == other.y();
+
+        } else {return false;}
+    }
+    @Override
+    public int hashCode() {
+        return x()*821^2 + y()*997^2;
+    }
+
+    @Override
     public String toString() {
         return x + "," + y;
+    }
+
+    @Override
+    public int compareTo(Coordinate other) {
+        int comparisionX = x() - other.x();
+        if (comparisionX == 0) {
+            return y() - other.y();
+        } else {return comparisionX;}
     }
 
 }
